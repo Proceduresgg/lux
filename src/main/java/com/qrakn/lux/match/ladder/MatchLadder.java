34701a8bad9964d65f644ec43ff77c84ec657ba6
Proceduresgg <DewgsIsGay143>
2019-04-22 14:31:23 -0600

    Begun implementing the casual match queue menu.

--- src/main/java/com/qrakn/lux/match/ladder/MatchLadder.java
+++ src/main/java/com/qrakn/lux/match/ladder/MatchLadder.java
@@ -1,4 +1,30 @@
 package com.qrakn.lux.match.ladder;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @Setter
+@RequiredArgsConstructor
 public class MatchLadder {
+
+    @Getter private static Map<String, MatchLadder> ladders = new HashMap<>();
+
+    private final String name;
+
+    private ItemStack icon = new ItemStack(Material.AIR);
+
+    public static MatchLadder createLadder(String name) {
+        MatchLadder ladder = new MatchLadder(name);
+
+        ladders.put(name, ladder);
+
+        return ladder;
+    }
 }
--- src/main/java/com/qrakn/lux/match/queue/menu/CasualMatchQueueMenu.java
+++ src/main/java/com/qrakn/lux/match/queue/menu/CasualMatchQueueMenu.java
@@ -1,8 +1,11 @@
 package com.qrakn.lux.match.queue.menu;
 
+import com.qrakn.lux.match.ladder.MatchLadder;
 import com.qrakn.phoenix.gui.menu.PlayerMenu;
 import com.qrakn.phoenix.gui.menu.item.MenuItem;
+import com.qrakn.phoenix.gui.menu.item.MenuItemBuilder;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 
 import java.util.List;
 
@@ -13,12 +16,16 @@ public class CasualMatchQueueMenu extends PlayerMenu {
     }
 
     @Override
-    public List<MenuItem> getItems(List<MenuItem> list) {
-        return null;
+    public List<MenuItem> getItems(List<MenuItem> items) {
+        MatchLadder.getLadders().values()
+                .forEach(ladder -> items.add(new MenuItemBuilder(ladder.getIcon().clone())
+                        .name(ladder.getName())
+                        .callback(ClickType.LEFT, () -> player.sendMessage(""))
+                        .build()));
+
+        return items;
     }
 
     @Override
-    protected void onClose() {
-
-    }
+    public void onClose() { }
 }
